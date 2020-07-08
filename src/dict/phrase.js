d8abe13288095b6776fc8580049d64a6f90dc019
daker <523246656@qq.com>
2020-07-08 10:12:18 +0800

    增加【商品】短语

--- src/dict/phrase.js
+++ src/dict/phrase.js
@@ -86,6 +86,101 @@ const phrase = [
     zh: "那么，你的意思是我们如今遇到大问题了。",
     en: "So, what you're saying is, we've got a big problem on our hands.",
     tag: [tags.确认]
+  },
+  {
+    zh: "我们绝对有必要见面。",
+    en: "It's absolutely critical that we meet.",
+    tag: [tags.重要性]
+  },
+  {
+    zh: "我们务必坐下来谈谈。",
+    en: "It's vital that we sit down together.",
+    tag: [tags.重要性, tags.约见面]
+  },
+  {
+    zh: "我们必须取得谅解。",
+    en: "It's essential we get an understanding.",
+    tag: [tags.重要性]
+  },
+  {
+    zh: "关键是她必须同意。",
+    en: "It's absolutely vital that she agree.",
+    tag: [tags.重要性]
+  },
+  {
+    zh: "我们正在在快速推进你的项目",
+    en: "We are fast-tracking your project.",
+    tag: [tags.工作]
+  },
+  {
+    zh: "您能快速查找我的申请吗？",
+    en: "Can you fast-track my application?",
+    tag: [tags.工作]
+  },
+  {
+    zh: "夜以继日的工作",
+    en: "working around the clock",
+    tag: [tags.工作, tags.短语]
+  },
+  {
+    zh: "随时告诉我们",
+    en: "keep us informed",
+    tag: [tags.短语]
+  },
+  {
+    zh: "落后计划",
+    en: "run behind schedule",
+    tag: [tags.短语]
+  },
+  {
+    zh: "离开",
+    en: "work out the door / leave",
+    tag: [tags.短语]
+  },
+  {
+    zh: "在短时的延迟后，我们回到了正常轨道。",
+    en: "After a short delay, we are back on track.",
+    tag: [tags.工作]
+  },
+  {
+    zh: "我今天有点落后于计划。",
+    en: "I'm running a little behind schedule today.",
+    tag: [tags.工作]
+  },
+  {
+    zh: "该相机没有附送配件。",
+    en: "The accessories don't come with the camera.",
+    tag: [tags.商品]
+  },
+  {
+    zh: "这台笔记本电脑物有所值。",
+    en: "This laptop is a good value for money.",
+    tag: [tags.商品]
+  },
+  {
+    zh: "它与其他平板电脑相比怎样？",
+    en: "And how does this compare with other tablets?",
+    tag: [tags.商品]
+  },
+  {
+    zh: "它有别的颜色吗？",
+    en: "Does it come in other colors?",
+    tag: [tags.商品]
+  },
+  {
+    zh: "它提供一个键盘吗？",
+    en: "Is it available with a keyboard?",
+    tag: [tags.商品]
+  },
+  {
+    zh: "",
+    en: "",
+    tag: [tags.商品]
+  },
+  {
+    zh: "",
+    en: "",
+    tag: [tags.商品]
   }
   // ,{
   //   zh: "",
